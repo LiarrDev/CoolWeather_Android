2d63cd7737a08b2fefa705c5cc87803979766f14
Liar <liarr@foxmail.com>
2018-07-07 15:08:42 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,46 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+
+	private int id;
+
+	private String cityName;        // 记录市的名字
+
+	private int cityCode;       // 记录市的代号
+
+	private int provinceId;     // 记录当前市所属的省的 id 值
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public int getCityCode() {
+		return cityCode;
+	}
+
+	public void setCityCode(int cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public int getProvinceId() {
+		return provinceId;
+	}
+
+	public void setProvinceId(int provinceId) {
+		this.provinceId = provinceId;
+	}
+}
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+
+	private int id;
+
+	private String provinceName;        // 记录省的名字
+
+	private int provinceCode;       // 记录省的代号
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public int getProvinceCode() {
+		return provinceCode;
+	}
+
+	public void setProvinceCode(int provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+}
